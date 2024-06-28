296a44ce1ab3d39665a73474bc07ed4a4fa52291
sunho <sunhome243@gmail.com>
2024-06-28 14:47:08 +0900

    secret setted

--- backend/user_service/database.py
+++ backend/user_service/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:Happy0702!@34.22.87.1/user_db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
