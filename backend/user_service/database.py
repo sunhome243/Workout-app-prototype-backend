23043b6ed80d54616045e8d7f6a6d94f56332f4b
sunho <sunhome243@gmail.com>
2024-06-28 17:18:05 +0900

    Update database.py

--- backend/user_service/database.py
+++ backend/user_service/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(SSQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
