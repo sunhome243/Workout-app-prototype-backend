d4b3de7a3e1d13f87cf238ef6bab7feb50fc0d13
sunho <sunhome243@gmail.com>
2024-07-02 15:13:38 +0900

    pyjwt problem

--- backend/user_service/utils.py
+++ backend/user_service/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Optional, Annotated
 import jwt
-from jwt import JWTError, decode
+from jwt.exceptions import InvalidTokenError
 import bcrypt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
